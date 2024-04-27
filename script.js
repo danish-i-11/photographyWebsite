215f5a99caf25344ecad1451c6eb094fc4816616
Danish Iqbal <diqbal885@gmail.com>
2024-04-27 11:36:38 +0530

    made all components except few

---
+++ script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+    $(window).on("scroll", function() {
+      if($(window).scrollTop() > 50) {
+          $("#header").addClass("active-header");
+          $(".menu-link").addClass("active-menu-links");
+          $("#header-img").attr("src", "./assets/danography.png");
+      } else {
+          //remove the background property so it comes transparent again (defined in your css)
+         $("#header").removeClass("active-header");
+         $(".menu-link").removeClass("active-menu-links");
+         $("#header-img").attr("src", "./assets/danography.png");
+      }
+  });
+  
+    // Responsive menu
+    $("#responsive-nav-bar").hide();
+  
+    $("#image-button-black").click(function(){
+      $("#responsive-nav-bar").slideToggle();
+    });
+  
+    $("#responsive-home-item , #responsive-features-item , #responsive-products-item , #responsive-media-item , #responsive-pricing-item").click(function(){
+      $("#responsive-nav-bar").slideToggle(200);
+    })
+  
+  });
+  
+
+
+
+
